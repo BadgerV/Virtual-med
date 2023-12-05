c1123c1fdcd33dd58ba947bb81495c5da5689818
Segunmaru Faozan <segunfaozan@gmail.com>
2023-12-05 21:42:03 +0100

    rettier😪

---
+++ frontend/src/components/SimpleAndFast/SimpleAndFast.jsx
@@ -0,0 +1,72 @@
+import "./simpleAndFast.css";
+
+const SimpleAndFast = () => {
+  return (
+    <div>
+      <div className="simple-and-fast">
+        <span className="simple-and-fast-header-text">Simple & Fast</span>
+        <span className="simple-and-fast-first-text">
+          4 easy steps to get your desired care and treatment
+        </span>
+
+        <div className="fast-list-of-services">
+          <div className="fast-list-service">
+            <img src="/assets/services-svg-1.svg" alt="" />
+
+            <div className="services-smaller-div">
+              <span className="services-smaller-header">
+                Check Doctor’s Profile
+              </span>
+
+              <span className="smaller-text">
+                Browse through our extensive catalog of experienced and
+                qualified medical practitioners.
+              </span>
+            </div>
+          </div>
+          <div className="fast-list-service">
+            <img src="/assets/services-svg-2.svg" alt="" />
+            <div className="services-smaller-div">
+              <span className="services-smaller-header">
+                Request Consultation
+              </span>
+
+              <span className="smaller-text">
+                Book a virtual appointment with your desired healthcare
+                personnel on our platform in just 3 easy steps.
+              </span>
+            </div>
+          </div>
+          <div className="fast-list-service">
+            <img src="/assets/services-svg-3.svg" alt="" />
+            <div className="services-smaller-div">
+              <span className="services-smaller-header">
+                Receive Consultation
+              </span>
+
+              <span className="smaller-text">
+                Have a virtual one on one consultation with your preferred
+                doctor at your convenience.
+              </span>
+            </div>
+          </div>
+          <div className="fast-list-service">
+            <img src="/assets/services-svg-4.svg" alt="" />
+            <div className="services-smaller-div">
+              <span className="services-smaller-header">
+                Get your desired help
+              </span>
+
+              <span className="smaller-text">
+                Get your desired solution, prescription or referral from our
+                doctors from the comfort of your own space!
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleAndFast;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,63 @@
+import SimpleAndFast from "../../components/SimpleAndFast/SimpleAndFast";
 import "./Home.css";
 
-import SwitchableButtons from "../../components/Button/Button";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
-// import { getDoctors } from "../../../redux/doctors/DoctorsSlice";
-
 const Home = () => {
-  return <div>this is what</div>;
+  return (
+    <>
+      <div className="home">
+        <div className="home-left">
+          <span className="home-big-text">
+            We provide digital healthcare for everyone, anywhere
+          </span>
+
+          <span className="home-small-text">
+            At VirtualMed, we strongly believe that everyone deserves access to
+            top-notch medical care, regardless of their location or schedule.
+          </span>
+
+          <div className="absolute-container">
+            <img src="/assets/ellipse-1.png" alt="doctor" />
+            <img
+              src="/assets/ellipse-2.png"
+              alt="doctor"
+              className="minus-margin"
+            />
+            <img
+              src="/assets/ellipse-3.png"
+              alt="doctor"
+              className="minus-margin"
+            />
+            <img
+              src="/assets/ellipse-4.png"
+              alt="doctor"
+              className="minus-margin"
+            />
+
+            <div className="absolute-cont-end-div">
+              <span className="home-first-text">50+</span>
+              <span className="home-second-text">Competent Doctors</span>
+            </div>
+          </div>
+
+          <div className="home-button-div">
+            <button className="home-first-button">Book appointment</button>
+            <button className="home-second-button">
+              Get Started <img src="/assets/right-arrow.png" alt="" />
+            </button>
+          </div>
+        </div>
+        <div className="home-right">
+          <img src="/assets/hero-image.png" alt="Hero Image" />
+        </div>
+      </div>
+
+      <div className="fast-and-simple-outer-container">
+        <div className="fast-and-simple-container">
+          <SimpleAndFast />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
